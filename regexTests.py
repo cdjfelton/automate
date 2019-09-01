d0d98e19e7e598e78a06b23f8712f0510abbfba2
chrisfelton <cdjfelton@gmail.com>
2019-09-01 10:26:47 -0500

    updates

---
+++ regexTests.py
@@ -0,0 +1,29 @@
+import re
+
+phoneNumRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
+mo = phoneNumRegex.search('My number is 414-333-4242.')
+print(mo.group(1))
+print(mo.group(2))
+print(mo.group(0))
+print(mo.group())
+print(mo.groups())
+areaCode, mainNumber = mo.groups()
+print("Area Code: " + areaCode)
+print("Main Number: " + mainNumber)
+
+# new section
+heroRegex = re.compile(r'Batman|Tina Fey')
+mo1 = heroRegex.search('Batman and Tina Fey.')
+print(mo1.group())
+mo2 = heroRegex.search('Tina Fey and Batman')
+print(mo2.group())
+
+batRegex = re.compile(r'Bat(man|mobile|copter|bat)')
+mo = batRegex.search('Batmobile lost a wheel')
+print(mo.group())
+
+batRegex = re.compile(r'Bat(wo)?man')
+mo1 = batRegex.search('The Adventures of Batman')
+print(mo1.group())
+mo2 = batRegex.search('The Adventures of Batwoman')
+print(mo2.group())
